13b258a787b4af7996ee48e0c813d8870608b026
Deddah777 <abdellahi.cheikhsidiya@ensea.fr>
2025-12-08 09:34:44 +0100

    tp_q1.c

---
+++ tp_q1.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <string.h>
+
+int main(){
+    // Text to display on start
+    const char *mess_prompt = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash%\n";
+
+    // Send text to stdout
+    write(STDOUT_FILENO, mess_prompt, strlen(mess_prompt));
+}
